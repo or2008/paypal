b7adce7253f8754a431db28a3d130b5d52f2c6ec
Or Bachar <or2008@gmail.com>
2018-12-19 17:12:55 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# paypal
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Dear Buyer!</h1>
+<div>
+  <h3>you can pay me now directly via Paypal - 118$</h3>
+  <br/>
+  send payment to <b>or2008@gmail.com</b> <br/>
+  or use this link:
+   <a href="https://www.paypal.me/OrBachar/118USD">https://www.paypal.me/OrBachar/118USD</a>
+
+  <br/>
+  <br/>
+   - Thank you, Or Bachar
+</div>
+`;
